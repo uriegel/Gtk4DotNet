d2f639cf99d0e14fa4376a37c2e0dda9eccff76e
Uwe Riegel <uriegel@hotmail.de>
2023-10-14 18:33:26 +0200

    PixBuf save scale

---
+++ 8-PixBuf/Program.cs
@@ -0,0 +1,27 @@
+﻿using GtkDotNet;
+
+using LinqTools;
+
+void SaveThumbnail(IntPtr _)
+    {
+    var pb = Pixbuf.NewFromFile(@"/home/uwe/20230908_095205.jpg");
+    Pixbuf.GetFileInfo(@"/home/uwe/20230908_095205.jpg", out var w, out var h);
+    var newh = 64 * h / w;
+    var thumbnail = Pixbuf.Scale(pb, 64, newh, Interpolation.Bilinear);
+    GObject.Unref(pb);
+    Pixbuf.SaveJpg(thumbnail, @"/home/uwe/thumbnail.jpg");
+    GObject.Unref(thumbnail);
+}
+
+return Application.Run("org.gtk.example", app => 
+    Application
+        .NewWindow(app)
+        .SideEffect(w => w.SetTitle("Hello Gtk PixBuf👍"))
+        .SideEffect(w => w.SetDefaultSize(1200, 1200))
+        .SideEffect(SaveThumbnail)
+        .Show());
+
+
+
+
+
--- Gtk4DotNet/Pixbuf.cs
+++ Gtk4DotNet/Pixbuf.cs
@@ -9,8 +9,24 @@ public class Pixbuf
     [DllImport(Globals.LibGtk, EntryPoint="gdk_pixbuf_new_from_file", CallingConvention = CallingConvention.Cdecl)]
     public extern static IntPtr NewFromFile(string fileName, IntPtr err);
 
+    public static bool SaveJpg(IntPtr pixbuf, string filename)
+    {
+        var error = IntPtr.Zero;
+        return Save(pixbuf, filename, "jpeg", ref error, IntPtr.Zero);
+    }
+        
+
     public static IntPtr NewFromResource(string resourcePath) => NewFromResource( resourcePath, IntPtr.Zero);
     [DllImport(Globals.LibGtk, EntryPoint="gdk_pixbuf_new_from_resource", CallingConvention = CallingConvention.Cdecl)]
     public extern static IntPtr NewFromResource(string resourcePath, IntPtr err);
+
+    [DllImport(Globals.LibGtk, EntryPoint="gdk_pixbuf_scale_simple", CallingConvention = CallingConvention.Cdecl)]
+    public extern static IntPtr Scale(IntPtr source, int width, int height, Interpolation interpolation);
+
+    [DllImport(Globals.LibGtk, EntryPoint="gdk_pixbuf_save", CallingConvention = CallingConvention.Cdecl)]
+    public extern static bool Save(IntPtr pixbuf, string filename, string type, ref IntPtr error, IntPtr nil);
+
+    [DllImport(Globals.LibGtk, EntryPoint="gdk_pixbuf_get_file_info", CallingConvention = CallingConvention.Cdecl)]
+    public extern static IntPtr GetFileInfo(string filename, out int width, out int height);
 }
 
