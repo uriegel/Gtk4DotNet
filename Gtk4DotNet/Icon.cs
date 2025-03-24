983795923053349583243feca2ffbbef8f8b9153
uriegel <uriegel@hotmail.de>
2025-03-24 18:13:55 +0100

    Bug

--- Gtk4DotNet/Icon.cs
+++ Gtk4DotNet/Icon.cs
@@ -11,7 +11,7 @@ public static class Icon
 
     public static IEnumerable<string> ThemedNames(this IconHandle icon)
     {
-        var stringArray = _IconToString(icon);
+        var stringArray = _ThemedGetNames(icon);
         int idx = 0;
         while (true)
         {
