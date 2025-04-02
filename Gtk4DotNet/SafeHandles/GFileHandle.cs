48d2487a6ad68f21cbdc051f6cb936f4abd3de27
uriegel <uriegel@hotmail.de>
2025-04-02 05:43:09 +0200

    Bug in file

--- Gtk4DotNet/SafeHandles/GFileHandle.cs
+++ Gtk4DotNet/SafeHandles/GFileHandle.cs
@@ -5,7 +5,4 @@ namespace GtkDotNet.SafeHandles;
 public class GFileHandle : ObjectHandle
 {
     public GFileHandle() : base() {}
-
-    protected override bool ReleaseHandle() 
-        => true.SideEffect(_ => GObject.Free(handle));
 }
