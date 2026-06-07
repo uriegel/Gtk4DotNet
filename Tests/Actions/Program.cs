86633ebc82c6f0fecdc0dda36076f4c780feab41
Uwe Riegel <uriegel@hotmail.de>
2026-06-07 18:35:58 +0200

    Actions start

---
+++ Tests/Actions/Program.cs
@@ -0,0 +1,13 @@
+﻿using Gtk4DotNet;
+
+Application
+    .New("de.uriegel.gtk4dotnet")
+    .WithDiagnostics()
+    .OnActivate(app => app
+        .NewWindow()
+        .Title("Hello World👍")
+        .DefaultSize(600, 200)
+        .Show()
+    ).Run();
+
+
