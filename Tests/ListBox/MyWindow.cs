e4a01f1db3fae08326023a9e984d10ecbad2721c
Uwe Riegel <uriegel@hotmail.de>
2026-06-13 00:06:39 +0200

    SetHeader

--- Tests/ListBox/MyWindow.cs
+++ Tests/ListBox/MyWindow.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 using Gtk4DotNet;
 
 // TODO Group items (like in Nautilus)
-
+// TODO Dictionary map nint to ListItem, onFinalize remove item from Dictionary
 class MyWindow : ApplicationWindow
 {
     public MyWindow(WindowBuilder builder) : base(builder)
@@ -15,6 +15,8 @@ class MyWindow : ApplicationWindow
         {
             Console.WriteLine($"{r}, {rp}");
             var item = GetListItem(r);
+            if (rp == 0)
+                SetHeader(r, Label.New("Das ist der tolle Hedder"));
         });
         foreach (var appinfo in appinfos.OrderBy(n => n.Name).Where(n => n.ShouldShow))
             listbox.AppendFromTemplate("listitem", b => new ListItem(b, appinfo.GetIcon(), appinfo.Name));
@@ -27,6 +29,10 @@ class MyWindow : ApplicationWindow
 
     [DllImport("libgtk-4.so.1", EntryPoint = "gtk_list_box_row_get_child", CallingConvention = CallingConvention.Cdecl)]
     extern static ListItem GetListItem(nint row);
+
+    [DllImport("libgtk-4.so.1", EntryPoint = "gtk_list_box_row_set_header", CallingConvention = CallingConvention.Cdecl)]
+    extern static void SetHeader(nint row, Widget header);
+    
 
 }
 
