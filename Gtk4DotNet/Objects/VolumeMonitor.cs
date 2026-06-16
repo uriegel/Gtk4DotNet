6f188f45542ad45778255b56d703423b1b88f40e
Uwe Riegel <uriegel@hotmail.de>
2026-06-16 21:14:19 +0200

    Mount signals

--- Gtk4DotNet/Objects/VolumeMonitor.cs
+++ Gtk4DotNet/Objects/VolumeMonitor.cs
@@ -19,6 +19,24 @@ public class VolumeMonitor : GObject
         => driveConnectedId = SignalConnect<ThreePointerDelegate>("drive-connected", (_, _, _) => onChanged(), true);
     public void OnDriveDisconnected(Action onChanged)
         => driveDisconnectedId = SignalConnect<ThreePointerDelegate>("drive-disconnected", (_, _, _) => onChanged(), true);
+    public void OnDriveEjectButton(Action onChanged)
+        => driveEjectButtonId = SignalConnect<ThreePointerDelegate>("drive-eject-button", (_, _, _) => onChanged(), true);
+    public void OnDriveStopButton(Action onChanged)
+        => driveStopButtonId = SignalConnect<ThreePointerDelegate>("drive-stop-button", (_, _, _) => onChanged(), true);
+    public void OnMountAdded(Action onChanged)
+        => mountAddedId = SignalConnect<ThreePointerDelegate>("mount-added", (_, _, _) => onChanged(), true);
+    public void OnMountChanged(Action onChanged)
+        => mountChangedId = SignalConnect<ThreePointerDelegate>("mount-changed", (_, _, _) => onChanged(), true);
+    public void OnMountPreUnmount(Action onChanged)
+        => mountPreUnmountId = SignalConnect<ThreePointerDelegate>("mount-pre-unmount", (_, _, _) => onChanged(), true);
+    public void OnMountRemoved(Action onChanged)
+        => mountRemovedId = SignalConnect<ThreePointerDelegate>("mount-removed", (_, _, _) => onChanged(), true);
+    public void OnVolumeAdded(Action onChanged)
+        => volumeAddedId = SignalConnect<ThreePointerDelegate>("volume-added", (_, _, _) => onChanged(), true);
+    public void OnVolumeChanged(Action onChanged)
+        => volumeChangedId = SignalConnect<ThreePointerDelegate>("volume-changed", (_, _, _) => onChanged(), true);
+    public void OnVolumeRemoved(Action onChanged)
+        => volumeRemovedId = SignalConnect<ThreePointerDelegate>("Volume-removed", (_, _, _) => onChanged(), true);
 
     [DllImport(Libs.LibGtk, EntryPoint = "g_volume_monitor_get", CallingConvention = CallingConvention.Cdecl)]
     extern static VolumeMonitor _Get();
@@ -26,6 +44,15 @@ public class VolumeMonitor : GObject
     DelegateId? driveChangeId = null;
     DelegateId? driveConnectedId = null;
     DelegateId? driveDisconnectedId = null;
+    DelegateId? driveEjectButtonId = null;
+    DelegateId? driveStopButtonId = null;
+    DelegateId? mountAddedId = null;
+    DelegateId? mountChangedId = null;
+    DelegateId? mountPreUnmountId = null;
+    DelegateId? mountRemovedId = null;
+    DelegateId? volumeAddedId = null;
+    DelegateId? volumeChangedId = null;
+    DelegateId? volumeRemovedId = null;
     
     #region IDisposable
 
@@ -42,6 +69,24 @@ public class VolumeMonitor : GObject
                 SignalDisconnect(driveConnectedId.Value);
             if (driveDisconnectedId.HasValue)
                 SignalDisconnect(driveDisconnectedId.Value);
+            if (driveEjectButtonId.HasValue)
+                SignalDisconnect(driveEjectButtonId.Value);
+            if (driveStopButtonId.HasValue)
+                SignalDisconnect(driveStopButtonId.Value);
+            if (mountAddedId.HasValue)
+                SignalDisconnect(mountAddedId.Value);
+            if (mountChangedId.HasValue)
+                SignalDisconnect(mountChangedId.Value);
+            if (mountPreUnmountId.HasValue)
+                SignalDisconnect(mountPreUnmountId.Value);
+            if (mountRemovedId.HasValue)
+                SignalDisconnect(mountRemovedId.Value);
+            if (volumeAddedId.HasValue)
+                SignalDisconnect(volumeAddedId.Value);
+            if (volumeChangedId.HasValue)
+                SignalDisconnect(volumeChangedId.Value);
+            if (volumeRemovedId.HasValue)
+                SignalDisconnect(volumeRemovedId.Value);
 
             // Nicht verwaltete Ressourcen (nicht verwaltete Objekte) freigeben und Finalizer überschreiben
             // Große Felder auf NULL setzen
--- Tests/Volumes/MyWindow.cs
+++ Tests/Volumes/MyWindow.cs
@@ -14,6 +14,15 @@ class MyWindow : ApplicationWindow
         monitor.OnDriveChanged(() => WriteLine("Drive changed"));
         monitor.OnDriveConnected(() => WriteLine("Drive connected"));
         monitor.OnDriveDisconnected(() => WriteLine("Drive disconnected"));
+        monitor.OnDriveEjectButton(() => WriteLine("Drive eject button"));
+        monitor.OnDriveStopButton(() => WriteLine("Drive stop button"));
+        monitor.OnMountAdded(() => WriteLine("Mount added"));
+        monitor.OnMountChanged(() => WriteLine("Mount changed"));
+        monitor.OnMountPreUnmount(() => WriteLine("Mount pre unmount"));
+        monitor.OnMountRemoved(() => WriteLine("Mount removed"));
+        monitor.OnVolumeAdded(() => WriteLine("Volume added"));
+        monitor.OnVolumeChanged(() => WriteLine("Volume changed"));
+        monitor.OnVolumeRemoved(() => WriteLine("Volume removed"));
 
         OnFinalize(monitor.Dispose);
     }
