215cf3fdc450e55e577794b4054697f0d08fcbf7
Uwe Riegel <uriegel@hotmail.de>
2026-06-21 22:03:17 +0200

    New app id

--- Tests/ExampleApp/Program.cs
+++ Tests/ExampleApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Gtk4DotNet;
 
 Application
-    .NewAdwaita("de.uriegel.gtk4dotnet", ApplicationFlags.HandlesOpen)
+    .NewAdwaita("de.uriegel.gtk4dotnet.example", ApplicationFlags.HandlesOpen)
     .WithDiagnostics(true)
     .OnOpen((app, files) =>
     {
