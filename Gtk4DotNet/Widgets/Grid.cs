d49b01ba0f587b1cd920250e443ca3e54f6d25ef
Uwe Riegel <uriegel@hotmail.de>
2026-06-24 08:03:33 +0200

    Packing Buttons

---
+++ Gtk4DotNet/Widgets/Grid.cs
@@ -0,0 +1,56 @@
+using System.Runtime.InteropServices;
+using CsTools.Extensions;
+
+namespace Gtk4DotNet;
+
+public class Grid : Widget
+{
+    public int RowSpacing
+    {
+        get => GetRowSpacing(this);
+        set => SetRowSpacing(this, value);
+    }
+    public int ColumnSpacing
+    {
+        get => GetColumnSpacing(this);
+        set => SetColumnSpacing(this, value);
+    }
+
+    public static Grid New()
+    {
+        var grid = _New();
+        grid.CheckDiagnostics();
+        return grid;
+    }
+    public Grid Attach(Widget widget, int column, int row, int columnSpan, int rowSpan)
+        => this.SideEffect(g => Attach(this, widget, column, row, columnSpan, rowSpan));
+
+    public Grid SetRowSpacing(int spacing)
+        => this.SideEffect(g => SetRowSpacing(this, spacing));
+
+    public Grid SetColumnSpacing(int spacing)
+        => this.SideEffect(g => SetColumnSpacing(this, spacing));
+
+    public Grid() : base() { }
+
+    public Grid(Builder builder, string? name = null) : base(builder, name) { }
+
+    [DllImport(Libs.LibGtk, EntryPoint="gtk_grid_new", CallingConvention = CallingConvention.Cdecl)]
+    extern static Grid _New();
+
+    [DllImport(Libs.LibGtk, EntryPoint = "gtk_grid_attach", CallingConvention = CallingConvention.Cdecl)]
+    extern static void Attach(Grid grid, Widget widget, int column, int row, int columnSpan, int rowSpan);
+
+    [DllImport(Libs.LibGtk, EntryPoint="gtk_grid_get_row_spacing", CallingConvention = CallingConvention.Cdecl)]
+    extern static int GetRowSpacing(Grid grid);
+
+    [DllImport(Libs.LibGtk, EntryPoint = "gtk_grid_set_row_spacing", CallingConvention = CallingConvention.Cdecl)]
+    extern static void SetRowSpacing(Grid grid, int spacing);
+
+    [DllImport(Libs.LibGtk, EntryPoint="gtk_grid_get_column_spacing", CallingConvention = CallingConvention.Cdecl)]
+    extern static int GetColumnSpacing(Grid grid);
+
+    [DllImport(Libs.LibGtk, EntryPoint="gtk_grid_set_column_spacing", CallingConvention = CallingConvention.Cdecl)]
+    extern static void SetColumnSpacing(Grid grid, int spacing);
+}
+
---
+++ Tests/PackButtons/Program.cs
@@ -0,0 +1,28 @@
+﻿using CsTools.Extensions;
+using Gtk4DotNet;
+
+using static System.Console;
+
+Application
+    .New("de.uriegel.gtk4dotnet")
+    .WithDiagnostics(true)
+    .OnActivate(app => app
+        .NewWindow()
+        .Title("Pack👍")
+        .Pipe(win => win.Child(
+            Grid
+                .New()
+                .Attach(                                
+                    Button
+                        .NewWithLabel("Button 1")
+                        .Clicked(() => WriteLine("Button1 clicked")), 0, 0, 1, 1)
+                .Attach(                                
+                    Button
+                        .NewWithLabel("Button 2")
+                        .Clicked(() => WriteLine("Button2 clicked")), 1, 0, 1, 1)
+                .Attach(                                
+                    Button
+                        .NewWithLabel("Quit")
+                        .Clicked(() => win.CloseWindow()), 0, 1, 2, 1)))
+        .Show()
+    ).Run();
