c449dd98730fbe7769d11a1427b3c2e2053fac56
Uwe Riegel <uriegel@hotmail.de>
2026-07-10 10:57:36 +0200

    TODO

--- Gtk4DotNet/Gtk/Gtk.cs
+++ Gtk4DotNet/Gtk/Gtk.cs
@@ -4,7 +4,11 @@ using Gtk4DotNet.Internals;
 
 namespace Gtk4DotNet;
 
+// TODO Test argvPtrs[i] = Marshal.StringToHGlobalAnsi(args[i]); with parameters with ä
+// TODO WebView test with location/index.html and /index.html
 // TODO GtkVideo with GtkMediaStream
+// TODO WebsiteFromResouce
+// TODO SendNotFound
 
 // TODO ListItem without placeholder box
 // TODO ProgressControlSubclassed without Box
