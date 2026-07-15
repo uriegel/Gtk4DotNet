e296693d19af8ed356146419475e5797569bc174
Uwe Riegel <uriegel@hotmail.de>
2026-07-15 21:42:17 +0200

    IsPlaying in MediaStream!

--- Gtk4DotNet/Interfaces.cs/IMediaStream.cs
+++ Gtk4DotNet/Interfaces.cs/IMediaStream.cs
@@ -1,6 +1,21 @@
+using System.Runtime.InteropServices;
+
 namespace Gtk4DotNet;
 
 public interface IMediaStream
 {
+    public bool IsPlaying
+    {
+        get => GetPlaying(GetRaw());
+        set => SetPlaying(GetRaw(), value);
+    }
+
     internal nint GetRaw();
+
+    [DllImport(Libs.LibGtk, EntryPoint = "gtk_media_stream_set_playing", CallingConvention = CallingConvention.Cdecl)]
+    extern static void SetPlaying(nint stream, bool val);
+
+    [DllImport(Libs.LibGtk, EntryPoint = "gtk_media_stream_get_playing", CallingConvention = CallingConvention.Cdecl)]
+    extern static bool GetPlaying(nint stream);
+
 }
--- Gtk4DotNet/Widgets/MediaControls.cs
+++ Gtk4DotNet/Widgets/MediaControls.cs
@@ -4,12 +4,6 @@ namespace Gtk4DotNet;
 
 public class MediaControls : Widget
 {
-    public bool IsPlaying
-    {
-        get => GetPlaying(this);
-        set => SetPlaying(this, value);
-    }
-    
     public void SetMediaStream(IMediaStream? stream)
         => SetMediaStream(this, stream?.GetRaw() ?? 0);
 
@@ -22,10 +16,4 @@ public class MediaControls : Widget
 
     [DllImport(Libs.LibGtk, EntryPoint = "gtk_media_controls_set_media_stream", CallingConvention = CallingConvention.Cdecl)]
     extern static void SetMediaStream(MediaControls contrls, nint stream);
-
-    [DllImport(Libs.LibGtk, EntryPoint = "gtk_media_stream_set_playing", CallingConvention = CallingConvention.Cdecl)]
-    extern static void SetPlaying(MediaControls contrls, bool val);
-
-    [DllImport(Libs.LibGtk, EntryPoint = "gtk_media_stream_get_playing", CallingConvention = CallingConvention.Cdecl)]
-    extern static bool GetPlaying(MediaControls contrls);
 }
