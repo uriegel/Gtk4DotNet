b2c4793ff04f8db774adf4284a4c61baa7deb273
Uwe Riegel <uriegel@hotmail.de>
2026-09-18 11:07:51 +0200

    AdwDialog CanClose

--- Gtk4DotNet/Widgets/AdwDialog.cs
+++ Gtk4DotNet/Widgets/AdwDialog.cs
@@ -21,6 +21,12 @@ public class AdwDialog : Widget
         dialog.Present(parent);
     }
 
+    public bool CanClose
+    {
+        get => GetCanClose(this);
+        set => SetCanClose(this, value);
+    }
+
     /// <summary>
     /// Shows the Adwaita dialog
     /// </summary>
@@ -41,7 +47,7 @@ public class AdwDialog : Widget
     public AdwDialog() : base() { }
 
     public AdwDialog(Builder builder, string? name = null) : base(builder, name) { }
-    
+
     [DllImport(Libs.LibAdw, EntryPoint = "adw_dialog_present", CallingConvention = CallingConvention.Cdecl)]
     extern static void Present(AdwDialog dialog, Widget parent);
 
@@ -51,4 +57,9 @@ public class AdwDialog : Widget
     [DllImport(Libs.LibAdw, EntryPoint = "adw_dialog_set_default_widget", CallingConvention = CallingConvention.Cdecl)]
     extern static void SetDefaultWidget(AdwDialog dialog, Widget widget);
 
+    [DllImport(Libs.LibAdw, EntryPoint = "adw_dialog_get_can_close", CallingConvention = CallingConvention.Cdecl)]
+    extern static bool GetCanClose(AdwDialog dialog);
+
+    [DllImport(Libs.LibAdw, EntryPoint = "adw_dialog_set_can_close", CallingConvention = CallingConvention.Cdecl)]
+    extern static void SetCanClose(AdwDialog dialog, bool val);
 }
